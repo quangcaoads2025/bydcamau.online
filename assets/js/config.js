6bdd7b44d3e700a62ae4d2a454113ce2c882e3fd
quangcaoads2025 <quangcaoads2025@gmail.com>
2026-07-30 14:18:51 +0700

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,55 @@
+'use strict';
+
+window.BYD_CONFIG = Object.freeze({
+  siteName: 'BYD Thành Công Cà Mau',
+  legalName: 'BYD Thành Công Cà Mau',
+  domain: 'https://bydcamau.online',
+  language: 'vi-VN',
+  currency: 'VND',
+  businessHours: '07:30 – 17:30, Thứ 2 – Chủ nhật',
+  hotlineSales: '0933570580',
+  confirmationHotline: '0848525352',
+  hotlineService: '0936570580',
+  email: 'bydthanhcong@bydthanhcongcm.vn',
+  zaloUrl: 'https://zalo.me/1506225984411083488',
+  messengerUrl: 'https://m.me/bydthanhcong.vn',
+  facebookUrl: 'https://www.facebook.com/bydthanhcong.vn/',
+  youtubeUrl: 'https://www.youtube.com/@BYDThànhCôngCàMau',
+  tiktokUrl: 'https://www.tiktok.com/@bydthanhcong',
+  formEndpoint: 'https://docs.google.com/forms/d/e/1FAIpQLSfV7hGUdxsPWhx6vxAo10RFjK1UdmzEPU61LTKqm11yCam1vA/formResponse',
+  googleFormFields: Object.freeze({
+    name: 'entry.1106886149',
+    phone: 'entry.1510643676',
+    vehicle: 'entry.1852135421',
+    area: 'entry.944411437'
+  }),
+  formCooldownSeconds: 5,
+  defaultArea: 'Cà Mau',
+  showrooms: {
+    showroom4s: {
+      id: 'showroom4s',
+      label: 'Showroom 4S',
+      name: 'BYD Thành Công Cà Mau – Showroom 4S',
+      address: 'Số 109, Đường Lý Thường Kiệt, Phường Tân Thành, Cà Mau',
+      mapQuery: 'BYD Thành Công Cà Mau 109 Lý Thường Kiệt Cà Mau'
+    }
+  },
+  seo: {
+    homeTitle: 'BYD Cà Mau - Giá lăn bánh, Ưu Đãi & Đăng Ký Lái Thử',
+    homeDescription: 'BYD Cà Mau cập nhật bảng giá, giá lăn bánh, ưu đãi và hỗ trợ trả góp các mẫu xe BYD mới nhất. Đăng ký lái thử, gọi 0848 52 53 52.',
+    ogImage: 'assets/images/byd-ca-mau-social-share.jpg'
+  },
+  financingDefaults: {
+    downPaymentPercent: 30,
+    annualRatePercent: 8.5,
+    termMonths: 84
+  },
+  registrationDefaults: {
+    registrationFeePercent: 0,
+    plateFee: 1000000,
+    roadMaintenanceFee: 1560000,
+    civilLiabilityInsurance: 480700,
+    inspectionFee: 140000,
+    serviceFee: 0
+  }
+});
