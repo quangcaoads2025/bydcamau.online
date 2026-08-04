d47a46f364e4d484071c09cb975fa8e1ce1de7c2
quangcaoads2025 <quangcaoads2025@gmail.com>
2026-08-04 11:13:24 +0700

    Update gtm-loader.js

--- assets/js/gtm-loader.js
+++ assets/js/gtm-loader.js
@@ -1,26 +1,25 @@
 'use strict';
 
-(() => {
-  const containerId = 'GTM-5NWJC98F';
+(function () {
+  const ID = 'GTM-5NWJC98F';
 
   if (window.__bydGtmLoaded) return;
-
   window.__bydGtmLoaded = true;
-  window.dataLayer = window.dataLayer || [];
 
+  window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({
     'gtm.start': Date.now(),
     event: 'gtm.js'
   });
 
   const script = document.createElement('script');
-
   script.async = true;
   script.src =
-    `https://www.googletagmanager.com/gtm.js?id=${encodeURIComponent(containerId)}`;
+    'https://www.googletagmanager.com/gtm.js?id=' +
+    encodeURIComponent(ID);
 
-  script.onerror = () => {
-    console.warn('[BYD] Google Tag Manager could not be loaded.');
+  script.onerror = function () {
+    console.warn('[BYD] Không tải được Google Tag Manager.');
   };
 
   document.head.appendChild(script);
